adca36b0a35bcb7bc8ca763529724a604d663e99
jdmlopes <jdlopes97@gmail.com>
2023-03-26 17:32:25 -0300

    Create the play Turn function

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ const ViewController = (function () {
 })();
 
 const gameController = (function () {
-  //TODO
+  let _turnPlayer;
+  let _player1;
+  let _player2;
+  let _winner = "";
+
+  const _switchTurnPlayer = () => {
+    if (_turnPlayer === _player1) {
+      _turnPlayer = _player2;
+    } else if (_turnPlayer === _player2) {
+      _turnPlayer = _player1;
+    }
+  };
+
+  const playTurn = (index) => {
+    Board.placeSymbol(_turnPlayer.getSymbol(), index);
+    _winner = Board.checkWinner();
+    if (_winner) {
+      //TODO CALL DISPLAY GAME OVER SCREEN
+      return;
+    }
+    _switchTurnPlayer();
+  };
+
+  return { playTurn };
 })();
 
 const Board = (function () {
@@ -23,12 +46,96 @@ const Board = (function () {
     _board[index] = symbol;
   };
 
-  return { getBoard, emptyBoard, placeSymbol };
+  const checkWinner = () => {
+    if (_board.find((symbol) => symbol === "") === undefined) {
+      return "tie";
+    }
+
+    if (
+      _board[0] !== "" &&
+      _board[0] === _board[1] &&
+      _board[1] === _board[2]
+    ) {
+      return _board[0];
+    }
+
+    if (
+      _board[3] !== "" &&
+      _board[3] === _board[4] &&
+      _board[4] === _board[5]
+    ) {
+      return _board[3];
+    }
+
+    if (
+      _board[6] !== "" &&
+      _board[6] === _board[7] &&
+      _board[7] === _board[8]
+    ) {
+      return _board[6];
+    }
+
+    if (
+      _board[0] !== "" &&
+      _board[0] === _board[3] &&
+      _board[3] === _board[6]
+    ) {
+      return _board[0];
+    }
+
+    if (
+      _board[1] !== "" &&
+      _board[1] === _board[4] &&
+      _board[4] === _board[7]
+    ) {
+      return _board[1];
+    }
+
+    if (
+      _board[2] !== "" &&
+      _board[2] === _board[5] &&
+      _board[5] === _board[8]
+    ) {
+      return _board[2];
+    }
+
+    if (
+      _board[0] !== "" &&
+      _board[0] === _board[4] &&
+      _board[4] === _board[8]
+    ) {
+      return _board[0];
+    }
+
+    if (
+      _board[2] !== "" &&
+      _board[2] === _board[4] &&
+      _board[4] === _board[6]
+    ) {
+      return _board[2];
+    }
+
+    return "segue o baile";
+  };
+
+  return { getBoard, emptyBoard, placeSymbol, checkWinner };
 })();
 
+function CreatePlayer(name, symbol) {
+  const _name = name;
+  const _symbol = symbol;
+
+  const getName = () => {
+    return _name;
+  };
+
+  const getSymbol = () => {
+    return _symbol;
+  };
+  return { getName, getSymbol };
+}
+
 Board.emptyBoard();
-Board.placeSymbol("X", 1);
-Board.placeSymbol("X", 5);
-Board.placeSymbol("O", 0);
 
 console.table(Board.getBoard());
+console.log(Board.checkWinner());
