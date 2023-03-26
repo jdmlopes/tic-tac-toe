b22ef9016f9ed3cef54731dfe84259a90f9db75a
jdmlopes <jdlopes97@gmail.com>
2023-03-26 14:29:58 -0300

    Create place symbol on board function

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ const gameController = (function () {
 })();
 
 const Board = (function () {
-  //TODO
+  const _board = [];
+
+  const getBoard = () => {
+    return _board;
+  };
+
+  const emptyBoard = () => {
+    _board.splice(0, _board.length);
+    for (let i = 0; i < 9; i++) _board.push("");
+  };
+
+  const placeSymbol = (symbol, index) => {
+    if (index >= _board.length || index < 0) return;
+    _board[index] = symbol;
+  };
+
+  return { getBoard, emptyBoard, placeSymbol };
 })();
+
+Board.emptyBoard();
+Board.placeSymbol("X", 1);
+Board.placeSymbol("X", 5);
+Board.placeSymbol("O", 0);
+
+console.table(Board.getBoard());
